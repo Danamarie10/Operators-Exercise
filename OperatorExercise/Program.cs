e99799a1fe7adf825bf3a3195550dd4a20925229
Danamarie10 <dana.patrick10@outlook.com>
2023-05-12 14:51:18 -0400

    Finished first exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,17 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            //Exercise 1 
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4);
+            Console.WriteLine($"{a} / {b} is 4 with a remainder of {remainder}");
         }
     }
 }
