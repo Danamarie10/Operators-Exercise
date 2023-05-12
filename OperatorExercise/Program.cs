0ce823baff713417af3c552a8ab031f24b9c636d
Danamarie10 <dana.patrick10@outlook.com>
2023-05-12 15:53:06 -0400

    Finished Exercise 2

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,18 +1,38 @@
-﻿namespace OperatorExercise
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace OperatorExercise
 {
     public class Program
     {
+
         static void Main()
         {
             //Exercise 1 
-            int a = 17;
-            int b = 4;
+            var a = 17;
+            var b = 4;
 
-            int quotient = a / b;
-            int remainder = a % b;
+            var quotient = a / b;
+            var remainder = a % b;
 
-            if (a == 17 && b == 4);
+            if (a == 17 && b == 4) ;
             Console.WriteLine($"{a} / {b} is 4 with a remainder of {remainder}");
+
+            Console.WriteLine("--------------------------------------------");
+
+            //Exercise 2
+
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+
+            double area = AreaOfCircle(radius);
+            Console.WriteLine($"The Area of the circle is: {area}");
+
+        }
+
+
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * radius * radius;
         }
     }
 }
